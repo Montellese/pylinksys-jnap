54fd8589a559ed7ec78fee104935f27023ed76c8
Sascha Montellese <sascha.montellese@scs.ch>
2023-01-23 23:30:26 +0100

    build: use pipenv to manage packages and scripts

---
+++ pylinksys_jnap/__init__.py
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2023 Sascha Montellese <sascha.montellese@gmail.com>
+# This file is part of pylinksys-jnap.
+#
+# SPDX-License-Identifier: Apache-2.0
+# See LICENSE for more information.
+#
+"""pylinksys-jnap"""
